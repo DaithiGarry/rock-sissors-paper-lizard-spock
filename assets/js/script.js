3427f7cc61ce828ae9d9a65bc3b96b0ff56c4fc8
David Garry <daithigarry@hotmail.com>
2022-06-10 00:48:19 +0000

    missing semi colons added

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
     userChoiceDisplay.innerHTML= userChoice;
     generateComputerChoice();
     getResult();
-}))
+}));
 });
 
 //function to generate computer choices during the game. Computer choices are generate automatically on the click of the users choice.
@@ -32,105 +32,105 @@ function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 5) + 1;
     
     if (randomNumber === 1) {
-      computerChoice = 'rock'
+      computerChoice = 'rock';
     }
     if (randomNumber === 2) {
-      computerChoice = 'scissors'
+      computerChoice = 'scissors';
     }
     if (randomNumber === 3) {
-      computerChoice = 'paper'
+      computerChoice = 'paper';
     }
     if (randomNumber === 4) {
-      computerChoice = 'lizard'
+      computerChoice = 'lizard';
     }
     if (randomNumber === 5) {
-      computerChoice = 'spock'
+      computerChoice = 'spock';
       }
     computerChoiceDisplay.innerHTML = computerChoice
-  };
+  }
 //function to get the result of the contest between the user and the computer.
 function getResult() {
   if (computerChoice === userChoice) {
-    result = 'its a draw!'
+    result = 'its a draw!';
     }
   if (computerChoice === 'scissors' && userChoice === "paper") {
-      result = 'you lost!'
+      result = 'you lost!';
       incrementincorrect();
     }
   if (computerChoice === 'paper' && userChoice === "rock") {
-      result = 'you lost!'
+      result = 'you lost!';
       incrementincorrect();
     }
   if (computerChoice === 'rock' && userChoice === "lizard") {
-      result = 'you lost!'
+      result = 'you lost!';
       incrementincorrect();
     }
   if (computerChoice === 'lizard' && userChoice === "spock") {
-      result = 'you lost!'
+      result = 'you lost!';
       incrementincorrect();
     }
   if (computerChoice === 'spock' && userChoice === "scissors") {
-      result = 'you lost!'
+      result = 'you lost!';
       incrementincorrect();
     }
   if (computerChoice === 'scissors' && userChoice === "lizard") {
-      result = 'you lost!'
+      result = 'you lost!';
       incrementincorrect();
     }
   if (computerChoice === 'lizard' && userChoice === "paper") {
-      result = 'you lost!'
+      result = 'you lost!';
       incrementincorrect();
     }
   if (computerChoice === 'paper' && userChoice === "spock") {
-      result = 'you lost!'
+      result = 'you lost!';
       incrementincorrect();
     }
   if (computerChoice === 'spock' && userChoice === "rock") {
-      result = 'you lost!'
+      result = 'you lost!';
       incrementincorrect();
     }
   if (computerChoice === 'rock' && userChoice === "scissors") {
-      result = 'you lost!'
+      result = 'you lost!';
       incrementincorrect();
     }
   if (computerChoice === 'paper' && userChoice === "scissors") {
-      result = 'you win!'
+      result = 'you win!';
       incrementcorrect();
     }
   if (computerChoice === 'rock' && userChoice === "paper") {
-      result = 'you win!'
+      result = 'you win!';
       incrementcorrect();
     }
   if (computerChoice === 'lizard' && userChoice === "rock") {
-      result = 'you win!'
+      result = 'you win!';
       incrementcorrect();
     }
   if (computerChoice === 'spock' && userChoice === "lizard") {
-      result = 'you win!'
+      result = 'you win!';
       incrementcorrect();
     }
   if (computerChoice === 'scissors' && userChoice === "spock") {
-      result = 'you win!'
+      result = 'you win!';
       incrementcorrect();
     }
   if (computerChoice === 'lizard' && userChoice === "scissors") {
-      result = 'you win!'
+      result = 'you win!';
       incrementcorrect();
     }
   if (computerChoice === 'paper' && userChoice === "lizard") {
-      result = 'you win!'
+      result = 'you win!';
       incrementcorrect();
     }
   if (computerChoice === 'spock' && userChoice === "paper") {
-      result = 'you win!'
+      result = 'you win!';
       incrementcorrect();
     }
   if (computerChoice === 'rock' && userChoice === "spock") {
-      result = 'you win!'
+      result = 'you win!';
       incrementcorrect();
     }
   if (computerChoice === 'scissors' && userChoice === "rock") {
-      result = 'you win!'
+      result = 'you win!';
       incrementcorrect();
     }
   resultDisplay.innerHTML = result;
@@ -159,7 +159,7 @@ resetGameButton.addEventListener("click", function() {
   computerChoiceDisplay.innerHTML = '';
   resultDisplay.innerHTML = '';
   userChoiceDisplay.innerHTML= '';
-})
+});
   
 
 
